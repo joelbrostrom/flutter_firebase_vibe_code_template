16c665f3715516e1961819dbac0f90530727c0fb
Joel Brostrom <joel.brostrom@appshack.se>
2026-03-18 17:34:52 +0100

    Init firebase when app starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'firebase_config.dart';
+import 'package:vibe_demo/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await initializeFirebase();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MainApp());
 }
